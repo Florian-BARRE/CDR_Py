9033176c228a344c26294a3bbfc29341a933b6a2
Florian BARRE <florian.barre78@gmail.com>
2022-11-18 17:14:28 +0100

    New markers display functions

--- ArUcO/classes/arucoRecognizer.py
+++ ArUcO/classes/arucoRecognizer.py
@@ -25,3 +25,81 @@ def draw_markers(frame: np.ndarray, detectobj: FrameDetection) -> np.ndarray:
     if detectobj.len:
         frame = cv2.aruco.drawDetectedMarkers(frame, detectobj.corners, detectobj.ids)
     return frame
+
+
+def _draw_circle_with_label(frame: np.ndarray, x: int, y: int, label: str, radius: int, thickness: int, color: tuple[int, int, int]) -> None:
+        cv2.circle(
+            frame,
+            (x, y),
+            radius,
+            color,
+            thickness
+        )
+
+        cv2.putText(
+            frame,
+            label,
+            (x+10, y+10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            color,
+            2
+        )
+
+def draw_corners(frame: np.ndarray, detectobj: FrameDetection, radius=5, thickness=2) -> np.ndarray:
+
+    # Corners deco
+    corners_deco = [
+        {"label": "A", "color": (255, 255, 0  )},
+        {"label": "B", "color": (0  , 255, 255)},
+        {"label": "C", "color": (255, 0  , 255)},
+        {"label": "D", "color": (255, 255, 255)},
+    ]
+    for index in range(detectobj.len):
+        for deco_index in range(len(corners_deco)):
+            _draw_circle_with_label(
+                frame,
+                int(detectobj.corners[index][0][deco_index][0]),
+                int(detectobj.corners[index][0][deco_index][1]),
+                corners_deco[deco_index]["label"],
+                radius,
+                thickness,
+                corners_deco[deco_index]["color"]
+            )
+
+    return frame
+
+
+def draw_barycenter(frame: np.ndarray, detectobj: FrameDetection, radius=5, thickness=2, color=(255, 0, 0)) -> np.ndarray:
+    for index in range(detectobj.len):
+        cv2.line(
+            frame,
+            (int(detectobj.corners[index][0][0][0]), int(detectobj.corners[index][0][0][1])),
+            (int(detectobj.corners[index][0][2][0]), int(detectobj.corners[index][0][2][1])),
+            color,
+            thickness
+        )
+        cv2.line(
+            frame,
+            (int(detectobj.corners[index][0][1][0]), int(detectobj.corners[index][0][1][1])),
+            (int(detectobj.corners[index][0][3][0]), int(detectobj.corners[index][0][3][1])),
+            color,
+            thickness
+        )
+
+        # Calcul Xcenter
+        x = (int(detectobj.corners[index][0][0][0]) + int(detectobj.corners[index][0][2][0]))//2
+        y = (int(detectobj.corners[index][0][0][1]) + int(detectobj.corners[index][0][2][1]))//2
+
+        _draw_circle_with_label(
+            frame,
+            x,
+            y,
+            f"X: {x}  Y:{y}",
+            radius,
+            thickness,
+            color
+        )
+
+    return frame
+
--- ArUcO/main.py
+++ ArUcO/main.py
@@ -1,5 +1,5 @@
 from classes.camera import Camera
-from classes.arucoRecognizer import ArucoRecognizer, draw_markers
+from classes.arucoRecognizer import ArucoRecognizer, draw_markers, draw_corners, draw_barycenter
 
 webcam = Camera()
 aruco_ia = ArucoRecognizer("DICT_4X4_100")
@@ -8,7 +8,12 @@ while True:
     frame = webcam.Read()
     founds = aruco_ia.detect(frame)
     marked_frame = draw_markers(frame, founds)
+    draw_corners(frame, founds)
+    draw_barycenter(frame, founds)
+
     webcam.Update_Monitor(marked_frame)
+
+
 
 """
 radius = 5

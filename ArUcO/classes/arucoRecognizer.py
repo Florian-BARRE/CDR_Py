9385e1257aa6399dc484bc8982e442b8d4e0ceee
Florian BARRE <florian.barre78@gmail.com>
2022-11-18 16:17:26 +0100

    Update arucoRecognizer to accept multi aruco code

--- ArUcO/classes/arucoRecognizer.py
+++ ArUcO/classes/arucoRecognizer.py
@@ -6,6 +6,7 @@ class FrameDetection:
     def __init__(self, ids: list[int], corners: list[np.array]):
         self.ids = ids
         self.corners = corners
+        self.len = 0 if ids is None else len(ids)
 
 
 class ArucoRecognizer:
@@ -21,61 +22,6 @@ class ArucoRecognizer:
 
 
 def draw_markers(frame: np.ndarray, detectobj: FrameDetection) -> np.ndarray:
-    if detectobj.ids:
+    if detectobj.len:
         frame = cv2.aruco.drawDetectedMarkers(frame, detectobj.corners, detectobj.ids)
     return frame
-
-
-"""
-
-webcam = Camera()
-arucoParams = cv2.aruco.DetectorParameters_create()
-arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
-
-
-while True:
-    frame = webcam.Read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, arucoDict, parameters=arucoParams)
-    cv2.aruco.drawDetectedMarkers(frame, corners)
-
-    #found = cv2.aruco.detectMarkers(frame, arucoDict, parameters=arucoParams)
-    #corners, ids, rejected_img_points = aruco.detectMarkers(gray, aruco_dict, parameters=parameters, cameraMatrix=matrix_coefficients, distCoeff=distortion_coefficients)
-
-    print(f"Corners: {corners}")
-    print(f"Ids: {ids}")
-    print(f"rejected_img_points: {rejected_img_points}")
-    print()
-
-    frame_markers = cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-
-    if corners:
-        radius = 5
-        thickness = 2
-
-        cv2.circle(
-            frame_markers,
-            (int(corners[0][0][0][0]), int(corners[0][0][0][1])),
-            radius,
-            (255, 0, 0),
-            thickness
-        )
-        cv2.circle(
-            frame_markers,
-            (int(corners[0][0][1][0]), int(corners[0][0][1][1])),
-            radius,
-            (0, 255, 0),
-            thickness
-        )
-        cv2.circle(
-            frame_markers,
-            (int(corners[0][0][2][0]), int(corners[0][0][2][1])),
-            radius,
-            (0, 0, 255),
-            thickness
-        )
-
-    webcam.Update_Monitor(frame_markers)
-
-"""
--- ArUcO/main.py
+++ ArUcO/main.py
@@ -8,5 +8,17 @@ while True:
     frame = webcam.Read()
     founds = aruco_ia.detect(frame)
     marked_frame = draw_markers(frame, founds)
-
     webcam.Update_Monitor(marked_frame)
+
+"""
+radius = 5
+thickness = 2
+
+cv2.circle(
+    frame_markers,
+    (int(corners[0][0][0][0]), int(corners[0][0][0][1])),
+    radius,
+    (255, 0, 0),
+    thickness
+)
+"""

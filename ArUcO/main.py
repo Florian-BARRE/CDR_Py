419d8ce7ca4ea5ebad6aaf203127751600a8cca8
Florian BARRE <florian.barre78@gmail.com>
2022-11-20 16:42:00 +0100

    camera distortion

--- ArUcO/main.py
+++ ArUcO/main.py
@@ -7,23 +7,8 @@ aruco_ia = ArucoRecognizer("DICT_4X4_100")
 while True:
     frame = webcam.Read()
     founds = aruco_ia.detect(frame)
-    marked_frame = draw_markers(frame, founds)
-    draw_corners(frame, founds)
+    #marked_frame = draw_markers(frame, founds)
+    #draw_corners(frame, founds)
     draw_barycenter(frame, founds)
 
-    webcam.Update_Monitor(marked_frame)
-
-
-
-"""
-radius = 5
-thickness = 2
-
-cv2.circle(
-    frame_markers,
-    (int(corners[0][0][0][0]), int(corners[0][0][0][1])),
-    radius,
-    (255, 0, 0),
-    thickness
-)
-"""
+    webcam.Update_Monitor(frame)

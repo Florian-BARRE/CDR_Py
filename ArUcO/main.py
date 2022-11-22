965f7c82f75b2badf3788c072492f5580846852c
Florian BARRE <florian.barre78@gmail.com>
2022-11-22 10:53:58 +0100

    correction de la distorsion de l'image

--- ArUcO/main.py
+++ ArUcO/main.py
@@ -7,8 +7,8 @@ aruco_ia = ArucoRecognizer("DICT_4X4_100")
 while True:
     frame = webcam.Read()
     founds = aruco_ia.detect(frame)
-    #marked_frame = draw_markers(frame, founds)
-    #draw_corners(frame, founds)
+    marked_frame = draw_markers(frame, founds)
+    draw_corners(frame, founds)
     draw_barycenter(frame, founds)
 
     webcam.Update_Monitor(frame)
